adb6f5fac646abd4bb12003b7a4305d0b1d3ec08
Rain <Rain@Lenovo-PC>
2018-11-06 12:04:20 -0500

    commit test 3

---
+++ KillerSheep/src/applications/Bernstein.java
@@ -0,0 +1,84 @@
+package applications;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import visual.dynamic.described.RuleBasedSprite;
+import visual.statik.sampled.TransformableContent;
+
+public class Bernstein extends RuleBasedSprite implements KeyListener
+{
+
+  protected double speed, x, y;
+
+  public Bernstein(TransformableContent content, double speed, double x, double y)
+  {
+    super(content);
+
+    this.x = x;
+    this.y = y;
+
+    setLocation(x, y);
+    this.speed = speed;
+  }
+
+  @Override
+  public void keyPressed(KeyEvent e)
+  {
+
+    switch (e.getKeyCode())
+    {
+      case KeyEvent.VK_UP:
+        y -= speed;
+        setLocation(x, y);
+        break;
+      case KeyEvent.VK_DOWN:
+        y += speed;
+        setLocation(x, y);
+        break;
+      case KeyEvent.VK_LEFT:
+        x -= speed;
+        setLocation(x, y);
+        break;
+      case KeyEvent.VK_RIGHT:
+        x += speed;
+        setLocation(x, y);
+        break;
+    }
+
+    // Somebody please refactor this to look less ugly. thanks.
+    if (x < 0)
+    {
+      x = 40;
+    }
+    if (x > 1050)
+    {
+      x = 1050;
+    }
+    if (y < 0)
+    {
+      y = 4.0;
+    }
+    if (y > 450)
+    {
+      y = 445;
+    }
+
+  }
+
+  @Override
+  public void handleTick(int arg0)
+  {
+    setLocation(x, y);
+  }
+
+  @Override
+  public void keyTyped(KeyEvent e)
+  {
+  }
+
+  @Override
+  public void keyReleased(KeyEvent e)
+  {
+  }
+}
--- KillerSheep/src/applications/KillerSheep.java
+++ KillerSheep/src/applications/KillerSheep.java
@@ -13,66 +13,81 @@ import app.JApplication;
 import io.ResourceFinder;
 import visual.Visualization;
 import visual.VisualizationView;
+import visual.dynamic.described.Stage;
 import visual.statik.sampled.BufferedImageOpFactory;
 import visual.statik.sampled.Content;
 import visual.statik.sampled.ContentFactory;
 
 public class KillerSheep extends JApplication
 {
-	
-	public KillerSheep(int width, int height) {
-		super(width, height);
-	}
 
-	@Override
-	public void init() 
-	{
-		// variable declarations
-		JPanel contentPane;
-		JLabel instructions;
-		ResourceFinder finder;
-		BufferedImageOpFactory opFactory;
-		Visualization visualization;
-		VisualizationView view;
-		
-		//content pane layout should be null
-		contentPane = (JPanel)getContentPane();
-		contentPane.setLayout(null);
-		
-		Border border = BorderFactory.createLineBorder(Color.BLACK, 1);
-		
-		// create the instructions panel
-		instructions = new JLabel();
-		instructions.setText("Use the arrow keys to direct Bernstein safely into the paddock! Avoid the angry, killer sheep!");
-		instructions.setFont(new Font("Serif", Font.PLAIN, 20));
-		instructions.setHorizontalAlignment(SwingConstants.CENTER);
-		instructions.setBorder(border);
-		instructions.setSize(800, 75);
-		instructions.setLocation(200, 700);
-		
-		//setup for background grass image
-		finder = ResourceFinder.createInstance(resources.Marker.class);
-		ContentFactory factory = new ContentFactory(finder);
-		opFactory = BufferedImageOpFactory.createFactory();
-		
-		Content grass = factory.createContent("grass.png", 3);
-		visualization = new Visualization();
-		grass.setLocation(0, 0);
-		grass.setBufferedImageOp(opFactory.createBlurOp(3));
-		visualization.add(grass);
-		
-		//add everything to contentPane
-		view = visualization.getView();
-		view.setSize(1200,700);
-		contentPane.add(view);
-		contentPane.add(instructions);
-		
-	}
-	
-	public static void main(String[] args)
-	{
-		KillerSheep app = new KillerSheep(1200,800);
-		invokeInEventDispatchThread(app);
-	}
+  public KillerSheep(int width, int height)
+  {
+    super(width, height);
+  }
+
+  @Override
+  public void init()
+  {
+    // variable declarations
+    JPanel contentPane;
+    JLabel instructions;
+    ResourceFinder finder;
+    BufferedImageOpFactory opFactory;
+    Visualization visualization;
+    VisualizationView view;
+
+    // content pane layout should be null
+    contentPane = (JPanel) getContentPane();
+    contentPane.setLayout(null);
+
+    Border border = BorderFactory.createLineBorder(Color.BLACK, 1);
+
+    // create the instructions panel
+    instructions = new JLabel();
+    instructions.setText(
+        "Use the arrow keys to direct Bernstein safely into the paddock! Avoid the angry, killer sheep!");
+    instructions.setFont(new Font("Serif", Font.PLAIN, 20));
+    instructions.setHorizontalAlignment(SwingConstants.CENTER);
+    instructions.setBorder(border);
+    instructions.setSize(500, 75);
+    instructions.setLocation(350, 600);
+
+    // setup for background grass image
+    finder = ResourceFinder.createInstance(resources.Marker.class);
+    ContentFactory factory = new ContentFactory(finder);
+    opFactory = BufferedImageOpFactory.createFactory();
+
+    /**
+     * Content grass = factory.createContent("grass.png", 3); visualization = new Visualization();
+     * grass.setLocation(0, 0); // grass.setBufferedImageOp(opFactory.createBlurOp(3));
+     * visualization.add(grass);
+     */
+
+    // Set up the stage with the grass background
+    Stage stage = new Stage(50);
+    Content grass = factory.createContent("grass.png", 3, false);
+    stage.add(grass);
+
+    // Add Berrnstein
+    Content bernstein = factory.createContent("b2.png", 4, false);
+    Bernstein b = new Bernstein(bernstein, 3, 40.0, 400.0);
+    stage.add(b);
+
+    // Make bernstein movable
+    stage.addKeyListener(b);
+
+    view = stage.getView();
+    view.setBounds(0, 0, this.width, 600);
+    contentPane.add(view);
+    contentPane.add(instructions);
+    stage.start();
+  }
+
+  public static void main(String[] args)
+  {
+    KillerSheep app = new KillerSheep(1200, 700);
+    invokeInEventDispatchThread(app);
+  }
 
 }
